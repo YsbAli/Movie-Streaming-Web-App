b2ef1b40c8d8d7dfa697f0e2eeedf044745fddec
Ysb <97525491+YsbAli@users.noreply.github.com>
2023-12-28 02:48:03 +0530

    Api data came

--- Frontend/src/pages/Netflix.jsx
+++ Frontend/src/pages/Netflix.jsx
@@ -3,19 +3,37 @@ import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import backgroundImage from "../assets/home.jpg";
 import MovieLogo from "../assets/homeTitle.webp";
-// import { onAuthStateChanged } from "firebase/auth";
-// import { firebaseAuth } from "../utils/firebase-config";
-// import { useNavigate } from "react-router-dom";
-// import { useSelector, useDispatch } from "react-redux";
-// import { fetchMovies, getGenres } from "../store";
+
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchMovies, getGenres } from "../store";
 import { FaPlay } from "react-icons/fa";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 // import Slider from "../components/Slider";
-import { useNavigate } from "react-router-dom";
-
-export default function Netflix() {
+function Netflix() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const movies = useSelector((state) => state.netflix.movies);
+  const genres = useSelector((state) => state.netflix.genres);
+  const genresLoaded = useSelector((state) => state.netflix.genresLoaded);
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getGenres());
+  }, []);
+
+  useEffect(() => {
+    if (genresLoaded) {
+      dispatch(fetchMovies({ genres, type: "all" }));
+    }
+  }, [genresLoaded]);
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (!currentUser) navigate("/login");
+  });
 
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
@@ -104,3 +122,4 @@ const Container = styled.div`
     }
   }
 `;
+export default Netflix;
