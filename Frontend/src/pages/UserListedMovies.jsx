057171669feb1110d220dc859735a9c3c7651c99
Ysb <97525491+YsbAli@users.noreply.github.com>
2023-12-28 21:03:48 +0530

    Done with Frontend Part

--- Frontend/src/pages/UserListedMovies.jsx
+++ Frontend/src/pages/UserListedMovies.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const UserListedMovies = () => {
   return (
     <div>
-      
+      <h1>Coming Soon...</h1>
     </div>
-  )
-}
+  );
+};
 
-export default UserListedMovies
+export default UserListedMovies;
